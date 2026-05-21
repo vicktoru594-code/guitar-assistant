ddd3e51da8b46bfdd34009765dbc2565b8c1f130
viks <kurochkina.viktoriy@mail.ru>
2026-05-21 12:11:47 +0700

    feat(tracker): add the function of recording practice and implement in-memory storage for practice sessions

--- src/tracker.h
+++ src/tracker.h
@@ -2,14 +2,20 @@
 #define TRACKER_H
 
 #include <string>
+#include <vector>
+#include <fstream>
 
 struct PracticeSession{
     std::string date;
-    int durationMitutes;
+    int durationMinutes;
     std::string notes;
 };
 
 void showTrackerMenu();
+void addPracticeSession(std::vector<PracticeSession>& sessions);
+void printHistory(const std::vector<PracticeSession>& sessions);
+void savePracticeToFile(const std::vector<PracticeSession>& sessions, const std::string& filename);
+std::vector<PracticeSession> loadPracticeFromFile(const std::string& filename);
 
 #endif
 
